0adbbabb68f2259427e8ffd2d63c3505d66d1fa5
Sinikeziwe03 <mdlivasinikeziwe@gmail.com>
2024-06-30 18:41:26 +0200

    updated star ratings

--- script.js
+++ script.js
@@ -1,44 +1,21 @@
-document.addEventListener("DOMContentLoaded",function(){
-let stars= document.querySelectorAll(".rating span i");
-let output= document.getElementById("output");
- 
-let rating= 0;
+let stars = document.querySelectorAll(".star i");
 
-function updatingRating(index){
-  rating= index;
-  output.innerHTML= `Rating is: ${rating}/5`;
+stars.forEach((star, index1) => {
 
- stars.forEach((star, i) => {
-   if (i < index) {
-     star.classList.remove("far"); // Remove empty star class
-     star.classList.add("fas"); // Add filled star class
-   } else {
-     star.classList.remove("fas"); // Remove filled star class
-     star.classList.add("far"); // Add empty star class
-   }
- });
+star.addEventListener("click", () => {
+
+stars.forEach((star, index2)=>{
+
+  index1 >= index2
+    ? star.classList.add("active")
+    : star.classList.remove("active");
+});
+});
+});
+
+function submitNow() {
+  alert("Thank you"+ " " + "for submitting your rating😊");
 }
+let submitNowButton = document.querySelector("button");
+submitNowButton.addEventListener("click", submitNow);
 
-stars.forEach((star, index) => {
-  star.addEventListener("click",() => {
-    updateRating(index + 1);
-    });
-});
-});
-
-document.querySelector(".button").addEventListener("click", function (event) {
-  event.preventDefault();
-
-  let name = document.querySelector(".name").value;
-  let email = document.querySelector(".email").value;
-  let message = document.querySelector(".message").value;
-
-  if (name && email && rating) {
-    alert(
-      `Thank you for your feedback!\nName: ${name}\nEmail: ${email}\nRating: ${rating}\nMessage: ${message}`
-    );
-    
-  } else {
-    alert("Please fill in all required fields and provide a rating.");
-  }
-});
